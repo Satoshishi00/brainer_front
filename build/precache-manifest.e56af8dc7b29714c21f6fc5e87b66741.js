191abe59213d4d5bb573397dd0a5dfaaf57ef29b
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-03-22 10:41:27 +0100

    add build directory

---
+++ build/precache-manifest.e56af8dc7b29714c21f6fc5e87b66741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd57592d13b7680a1340695a1cb62ff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0cd22a1660ff0e0c08e",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "bd199bd4a09c21c7eb73",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0cd22a1660ff0e0c08e",
+    "url": "/static/js/2.20065765.chunk.js"
+  },
+  {
+    "revision": "25a56c92b4beb4606458a48a046f101e",
+    "url": "/static/js/2.20065765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd199bd4a09c21c7eb73",
+    "url": "/static/js/main.b84d182e.chunk.js"
+  },
+  {
+    "revision": "890c04a74498a7f2ee9d",
+    "url": "/static/js/runtime-main.11fcc782.js"
+  }
+]);
