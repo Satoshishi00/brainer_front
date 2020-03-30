94d5d868c5be5595d675f4f3d3251762b7b4d373
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-03-30 17:49:25 +0200

    récupération des fichier du dossier build

---
+++ build/precache-manifest.a08a9ac8960366c00ebab1e8022bbc82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d63da0d413d76a702af81f7e09cfba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d56d46ec898aae2c8cb",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "f1eb7d7d1146d28d3d64",
+    "url": "/static/css/main.871b64d9.chunk.css"
+  },
+  {
+    "revision": "1d56d46ec898aae2c8cb",
+    "url": "/static/js/2.7d13cc0b.chunk.js"
+  },
+  {
+    "revision": "25a56c92b4beb4606458a48a046f101e",
+    "url": "/static/js/2.7d13cc0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1eb7d7d1146d28d3d64",
+    "url": "/static/js/main.5be35c81.chunk.js"
+  },
+  {
+    "revision": "890c04a74498a7f2ee9d",
+    "url": "/static/js/runtime-main.11fcc782.js"
+  }
+]);
